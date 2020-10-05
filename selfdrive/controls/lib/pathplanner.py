0de5e59c167b5c058509f5f5f6b5934323b50621
lispain <68838103+lispain@users.noreply.github.com>
2020-10-05 13:22:53 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -302,7 +302,7 @@ class PathPlanner():
       elif self.lane_change_state == LaneChangeState.laneChangeStarting:
         # fade out over .5s
         xp = [40,80]
-        fp2 = [0.2,2]
+        fp2 = [0.5,2]
         lane_time = interp( v_ego_kph, xp, fp2 )        
         self.lane_change_ll_prob = max(self.lane_change_ll_prob - lane_time*DT_MDL, 0.0)
         # 98% certainty
