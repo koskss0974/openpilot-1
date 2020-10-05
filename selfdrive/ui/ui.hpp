facc42140d85a4e4400db7c36e6997d6dcd59b16
lispain <68838103+lispain@users.noreply.github.com>
2020-10-05 13:50:20 +0900

    Update ui.hpp

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -174,7 +174,7 @@ typedef struct UIScene {
   cereal::HealthData::HwType hwType;
   int satelliteCount;
   uint8_t athenaStatus;
-
+  int    nTimer;
 
   // pathcoloring
   struct _KEGMEN_
@@ -267,6 +267,8 @@ typedef struct UIScene {
     int nLightSensor;
   } params;
 
+  
+  
 } UIScene;
 
 typedef struct {
