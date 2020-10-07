652674cfb4a81c0afbc126c414f009fc0e1db613
lispain <68838103+lispain@users.noreply.github.com>
2020-10-08 06:51:13 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -18,6 +18,8 @@ import cereal.messaging as messaging
 from selfdrive.loggerd.config import get_available_percent
 from selfdrive.pandad import get_expected_signature
 from selfdrive.thermald.power_monitoring import PowerMonitoring, get_battery_capacity, get_battery_status, get_battery_current, get_battery_voltage, get_usb_present
+import re
+import subprocess
 
 FW_SIGNATURE = get_expected_signature()
 
